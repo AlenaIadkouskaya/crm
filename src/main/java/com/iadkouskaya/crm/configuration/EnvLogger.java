69e412624b52876b0efd803e6b108e665009c3be
Alena Iadkouskaya <eiodkovskaya6@gmail.com>
2025-09-13 13:25:05 +0200

    EnvLogger

--- src/main/java/com/iadkouskaya/crm/configuration/EnvLogger.java
+++ src/main/java/com/iadkouskaya/crm/configuration/EnvLogger.java
@@ -3,22 +3,27 @@ package com.iadkouskaya.crm.configuration;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Component
 public class EnvLogger {
-    @Value("${SPRING_DATASOURCE_URL:NOT SET}")
+    private static final Logger logger = LoggerFactory.getLogger(EnvLogger.class);
+
+    @Value("${spring.datasource.url:NOT SET}")
     private String datasourceUrl;
 
-    @Value("${SPRING_DATASOURCE_USERNAME:NOT SET}")
+    @Value("${spring.datasource.username:NOT SET}")
     private String datasourceUsername;
 
-    @Value("${SPRING_DATASOURCE_PASSWORD:NOT SET}")
+    @Value("${spring.datasource.password:NOT SET}")
     private String datasourcePassword;
 
     @PostConstruct
     public void logEnv() {
-        System.out.println("SPRING_DATASOURCE_URL = " + datasourceUrl);
-        System.out.println("SPRING_DATASOURCE_USERNAME = " + datasourceUsername);
-        System.out.println("SPRING_DATASOURCE_PASSWORD = " + (datasourcePassword.equals("NOT SET") ? "NOT SET" : "***"));
+        logger.info("spring.datasource.url = {}", datasourceUrl);
+        logger.info("spring.datasource.username = {}", datasourceUsername);
+        logger.info("spring.datasource.password = {}", datasourcePassword.equals("NOT SET") ? "NOT SET" : "***");
     }
 }
